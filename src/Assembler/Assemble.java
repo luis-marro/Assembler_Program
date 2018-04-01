13c4df00de8b4628fa8a96a8b50c07cb7b605364
Luis Fernando Marroquin Martinez <luisfer.marroquin@hotmail.com>
2018-04-01 17:21:47 -0600

    Added and tested methods for the first and second swipe steps in the translation.

--- src/Assembler/Assemble.java
+++ src/Assembler/Assemble.java
@@ -1,10 +1,16 @@
 package Assembler;
 
+import com.sun.deploy.util.StringUtils;
+
 import java.util.*;
 import java.io.*;
 
 /**
  * Created by luisf on 25/03/2018.
+ */
+
+/**
+ * This is the class that handles all the necessary logic to translate assembler code to machine language.
  */
 public class Assemble {
     private List<String> lines;
@@ -18,7 +24,9 @@ public class Assemble {
     private List<String> assemblerCode;
     private List<String> binaryCode;
     // Hash table to save the memory Addresses
-    private HashMap<String, Short> translateTable;
+    private HashMap<String, Short> symbolTable;
+    // counter to keep position in ram
+    private short posInTable;
 
     /**
      * Constructor fot the assembler class
@@ -31,25 +39,26 @@ public class Assemble {
         destRules = new HashMap<String, String>();
         assemblerCode = new ArrayList<String>();
         binaryCode = new ArrayList<String>();
-        translateTable = new HashMap<String, Short>();
-        readRules();
+        symbolTable = new HashMap<String, Short>();
         initializeValues();
+        posInTable = 16;
+        readRules();
     }
 
     /**
      * This method Initializes the tranlation table with common addresses between all assembler code
      */
     private void initializeValues(){
-        translateTable.put("SP", (short)0);
-        translateTable.put("LCL", (short)1);
-        translateTable.put("ARG", (short)2);
-        translateTable.put("THIS", (short)3);
-        translateTable.put("THAT", (short)4);
+        symbolTable.put("SP", (short)0);
+        symbolTable.put("LCL", (short)1);
+        symbolTable.put("ARG", (short)2);
+        symbolTable.put("THIS", (short)3);
+        symbolTable.put("THAT", (short)4);
         for(int i = 0; i < 16; i++){
-            translateTable.put("R"+Integer.toString(i), (short)i);
+            symbolTable.put("R"+Integer.toString(i), (short)i);
         }
-        translateTable.put("SCREEN", (short)16384);
-        translateTable.put("KBD", (short)24576);
+        symbolTable.put("SCREEN", (short)16384);
+        symbolTable.put("KBD", (short)24576);
     }
 
     /**
@@ -124,20 +133,15 @@ public class Assemble {
                         // split the string with the comment separator
                         String[] getComments = line.split("//");
                         // the first position of the array should be the instruction
-                        // it can contain blank spaces, delete them
-                        getComments[0] = getComments[0].replace(" ", "");
-                        getComments[0] = getComments[0].trim();
+                        getComments[0] = StringUtils.trimWhitespace(getComments[0]);
                         assemblerCode.add(getComments[0]);
                     } else {
                         // no comments, just clean and add the instruction
                         line = line.replace(" ", "");
-                        line = line.trim();
+                        line = StringUtils.trimWhitespace(line);
                         assemblerCode.add(line);
                     }
                 }
-            }
-            for(int i = 0; i < assemblerCode.size(); i++){
-                System.out.println(assemblerCode.get(i));
             }
             return true;
         }catch (FileNotFoundException e){
@@ -155,14 +159,44 @@ public class Assemble {
      */
     public void translate(String path){
         readFile(path);
-
+        firstSwipe();
+        secondSwipe();
 
     }
 
     /**
-     * This method performs the first swipe on the data, adds tags to the translateTable 
+     * This method performs the first swipe on the data, adds tags to the symbolTable
      */
     private void firstSwipe(){
+        // traverse the assembler code searching for tags, as they should be stored first in the translate table
+        for(short i = 0; i < assemblerCode.size(); i++){
+            if(assemblerCode.get(i).startsWith("(")){
+                String tag = StringUtils.trimWhitespace(assemblerCode.get(i).replace("(", "").replace(")", ""));
+                symbolTable.put(tag, (short)(i+1));
+            }
+        }
+    }
 
+    /**
+     * This method performs the second swipe on the file, which should save and translate all variables of a Instructions
+     */
+    private void secondSwipe(){
+        // traverse the assembler code searching for a type instructions
+        for(int i = 0; i < assemblerCode.size(); i++){
+            if(assemblerCode.get(i).startsWith("@")){
+                // Check if the value is a variable or a tag
+                String value = StringUtils.trimWhitespace(assemblerCode.get(i).replace("@", ""));
+                // if the value exists in Symbol table, it is a tag, otherwise, a variable.
+                if(symbolTable.containsKey(value)){
+                    // Get the value of the tag and replace it in the assembler code.
+                    assemblerCode.set(i, "@" + symbolTable.get(value).toString());
+                }else {
+                    // the value is a variable, add it to symbol table and update it in code
+                    symbolTable.put(value, posInTable);
+                    assemblerCode.set(i, "@" + Integer.toString(posInTable));
+                    posInTable++;
+                }
+            }
+        }
     }
 }
